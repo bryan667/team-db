33adb23c1e9bfbdf7093bbf0a502d8d4c0204691
Bryan M <janbryanmartirez@gmail.com>
2018-09-25 21:17:17 +0800

    added mid

--- src/components/home/featured/index.js
+++ src/components/home/featured/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stripes from './stripes'
+import Text from './text'
 
 const Featured = () => {
     return (
         <div className='featured_wrapper'>
             <Stripes></Stripes>
+            <Text></Text>
         </div>
     );
 };
