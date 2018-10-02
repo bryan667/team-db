f727c5a77d2845c3bdd1dc9d1666a51d0a1739ed
Bryan M <janbryanmartirez@gmail.com>
2018-10-02 08:55:24 +0800

    edited public and private routes

--- src/high-order-comp/admin-layout.js
+++ src/high-order-comp/admin-layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AdminNav from '../components/admin/nav/admin-nav'
 
 const AdminLayout = (props) => {
     return (
         <div className='admin_container'>
             <div className='admin_left_nav'>
-                nav
+                <AdminNav></AdminNav>
             </div>
             <div className='admin_right'>
                 {props.children}
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Layout from './high-order-comp/Layout';
-import {Switch,Route} from 'react-router-dom'
+import {Switch} from 'react-router-dom'
 import Home from './components/home/index'
 import SignIn from './components/sign_in/index'
 import Dashboard from './components/admin/dashboard'
+import PrivateRoutes from './components/authRoutes/privateRoutes'
+import PublicRoutes from './components/authRoutes/publicRoutes'
 
 
-const Routes = props => {
+const Routes = (props) => {
+
+
   return (
       <Layout>
         <Switch>
-          <Route exact component={Home} path='/'></Route>
-          <Route exact component={SignIn} path='/sign_in'></Route>
-          <Route exact component={Dashboard} path='/dashboard'></Route>
+          <PublicRoutes {...props} restricted={true} path='/sign_in' exact component={SignIn}></PublicRoutes>
+          <PublicRoutes {...props} restricted={false} path='/' exact component={Home}></PublicRoutes>
+          <PrivateRoutes {...props} path='/dashboard' exact component={Dashboard}></PrivateRoutes>
         </Switch>
       </Layout>
   )
