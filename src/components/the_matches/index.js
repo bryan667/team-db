13519505f264fbe51ccf8d9762379a5c100c68da
Bryan M <janbryanmartirez@gmail.com>
2018-10-08 09:25:48 +0800

    the_matches

--- src/components/the_matches/index.js
+++ src/components/the_matches/index.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 import {firebaseMatches} from '../../firebase-db'
-import { firebaseForEach, reverseArray } from "../ui/misc";
+import { firebaseForEach, reverseArray } from "../ui/misc"
+
+import LeagueTable from './table.js'
+import MatchesList from './matches_list'
+
+
 
 class TheMatches extends Component {
+
+    state = {
+        loading: true,
+        matches: [],
+        filterMatches: [],
+        playerFilter: 'All',
+        resultFilter: 'All'
+    }
+
+    componentDidMount() {
+        firebaseMatches.once('value').then((snap)=> {
+            const matches = firebaseForEach(snap)
+            this.setState({
+                loading: false,
+                matches: reverseArray(matches),
+                filterMatches: reverseArray(matches)
+            })
+        })
+    }
+
+
     render() {
+        const state = this.state
         return (
-            <div>
-                the matches
+            <div className='the_matches_container'>
+                <div className='the_matches_wrapper'>
+                    <div className='left'>
+                        <div className='match_filters'>
+
+                        </div>
+                        {console.log(this.state.filterMatches)}
+                        <MatchesList matches={this.state.filterMatches}/>
+                    </div>
+                    <div className='right'>
+                        <LeagueTable/>
+                    </div>
+
+                </div>
             </div>
         );
     }
